73f5811c26b9145ada632c8db7398e9fcf65f6e5
Nate River <hippogamesunity@gmail.com>
2026-03-16 19:18:33 +0300

    remove old scripts

--- Steam/Fantasy Heroes 4D.bat
+++
@@ -1,1 +0,0 @@
-builder\steamcmd.exe +login hippogamesunity lthgfhjkm7_ +run_app_build ..\scripts\app_build_1741230.vdf
--- Steam/Pixel Studio Mac.bat
+++
@@ -1,1 +0,0 @@
-builder\steamcmd.exe +login hippogamesunity lthgfhjkm7_ +run_app_build_http ..\scripts\app_build_1204050_mac.vdf
